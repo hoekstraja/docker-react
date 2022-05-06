5c9cfda89b341c89802f1785fc77b01c05e3b0a5
jellehoekstra <jahoekstra@ziggo.nl>
2022-05-06 13:11:15 +0200

    app.js changed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Jelle. doei doei gewijzigd
+          Hello Jelle. leuke zooi
         </p>
         <a
           className="App-link"
